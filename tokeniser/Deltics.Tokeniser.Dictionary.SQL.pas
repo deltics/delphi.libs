b3eeba8df9fc39b91e8ca1cfd47c9cdd58b2a31b
Jolyon Smith <jsmith@deltics.co.nz>
2014-05-06 11:06:26 +1200

    Initial dictionary for SQL

---
+++ tokeniser/Deltics.Tokeniser.Dictionary.SQL.pas
@@ -0,0 +1,167 @@
+
+
+  unit Deltics.Tokeniser.Dictionary.SQL;
+
+
+interface
+
+  uses
+    Deltics.Tokeniser.Consts,
+    Deltics.Tokeniser.Dictionary;
+
+
+  var
+    CoreSQL: TTokenDictionary = NIL;
+
+
+  const
+    ttComment       = #3;
+    ttReservedWord  = #4;
+    ttIdentifier    = #5;
+    ttLiteral       = #6;
+    ttSymbol        = #7;
+
+    sqlWhitespace         = 100;
+    sqlEOL                = 101;
+
+    sqlBegin              = 601;
+    sqlCursor             = 602;
+    sqlDeclare            = 603;
+    sqlDELETE             = 604;
+    sqlINSERT             = 608;
+    sqlJOIN               = 609;
+    sqlFROM               = 610;
+    sqlINTO               = 611;
+    sqlPrepare            = 612;
+    sqlSection            = 613;
+    sqlSET                = 614;
+    sqlSELECT             = 615;
+    sqlUPDATE             = 616;
+    sqlWHERE              = 617;
+    sqlLEFT               = 618;
+    sqlINNER              = 619;
+    sqlRIGHT              = 620;
+    sqlOUTER              = 621;
+    sqlFOR                = 622;
+    sqlOF                 = 623;
+    sqlON                 = 624;
+    sqlIS                 = 625;
+    sqlNOT                = 626;
+    sqlNULL               = 627;
+    sqlINDEX              = 628;
+    sqlCREATE             = 629;
+    sqlALTER              = 630;
+    sqlGRANT              = 631;
+    sqlCALL               = 632;
+    sqlCOMMENT            = 633;
+
+    sqlQuotedIdentifier   = 700;
+    sqlIdentifier         = 701;
+    sqlInteger            = 702;
+    sqlFloat              = 703;
+    sqlString             = 704;
+
+    sqlPeriod             = tkPeriod;
+    sqlComma              = tkComma;
+    sqlEquals             = tkEquals;
+    sqlLessThan           = tkLeftAngleBrace;
+    sqlGreaterThan        = tkRightAngleBrace;
+    sqlOpenParenthesis    = tkLeftParenthesis;
+    sqlCloseParenthesis   = tkRightParenthesis;
+
+
+  type
+    TSQLDictionary = class(TTokenDictionary)
+      procedure Initialise; override;
+    end;
+
+
+
+implementation
+
+{ TSQLDictionary }
+
+  procedure TSQLDictionary.Initialise;
+  var
+    charSets: TArrayOfANSICharSet;
+  begin
+    SetCaseSensitivity(FALSE);
+    SetName('COBOL Embedded SQL');
+
+    TokenType := ttWhitespace;
+    AddCharSet(sqlWhitespace, '[whitespace]', [' ', #9]);
+    AddString(sqlEOL, '[CR]',    #13);
+    AddString(sqlEOL, '[LF]',    #10);
+    AddString(sqlEOL, '[CRLF]',  #13#10);
+
+    TokenType := ttIdentifier;
+    AddCharSet(sqlIdentifier,         'identifier', ['a'..'z', '0'..'9',  '_']);
+    AddDelimited(sqlQuotedIdentifier, 'quoted identifier', '"', '"');
+
+    TokenType := ttLiteral;
+    AddDelimited(sqlString,   'string', '''', '''');
+
+    SetLength(charSets, 2);
+    charSets[0] := ['-', '+', '0'..'9'];
+    charSets[1] := ['0'..'9'];
+    AddCharSet(sqlInteger, 'Integer', charSets, 1);
+
+    SetLength(charSets, 7);
+    charSets[0] := ['-', '+', '0'..'9'];
+    charSets[1] := ['0'..'9'];
+    charSets[2] := ['.'];
+    charSets[3] := ['0'..'9'];
+    charSets[4] := ['e'];
+    charSets[5] := ['+', '-', '0'..'9'];
+    charSets[6] := ['0'..'9'];
+    AddCharSet(sqlFloat, 'Float', charSets, [3,6]);
+
+    TokenType := ttReservedWord;
+    AddString(sqlBegin,    'begin');
+    AddString(sqlDeclare,  'declare');
+    AddString(sqlSection,  'section');
+    AddString(sqlFROM,     'from');
+    AddString(sqlINTO,     'into');
+    AddString(sqlJOIN,     'join');
+    AddString(sqlSELECT,   'select');
+    AddString(sqlSET,      'set');
+    AddString(sqlINSERT,   'insert');
+    AddString(sqlUPDATE,   'update');
+    AddString(sqlDELETE,   'delete');
+    AddString(sqlLEFT,     'left');
+    AddString(sqlINNER,    'inner');
+    AddString(sqlRIGHT,    'right');
+    AddString(sqlOUTER,    'outer');
+    AddString(sqlWHERE,    'where');
+    AddString(sqlFOR,      'for');
+    AddString(sqlOF,       'of');
+    AddString(sqlON,       'on');
+    AddString(sqlIS,       'is');
+    AddString(sqlNOT,      'not');
+    AddString(sqlNULL,     'null');
+    AddString(sqlCREATE,   'create');
+    AddString(sqlINDEX,    'index');
+    AddString(sqlALTER,    'alter');
+    AddString(sqlGRANT,    'grant');
+    AddString(sqlCALL,     'call');
+    AddString(sqlCOMMENT,  'comment');
+
+    TokenType := ttSymbol;
+    AddASCII(sqlComma);
+    AddASCII(sqlPeriod);
+    AddASCII(sqlOpenParenthesis);
+    AddASCII(sqlCloseParenthesis);
+    AddASCII(sqlEquals);
+    AddASCII(sqlLessThan);
+    AddASCII(sqlGreaterThan);
+  end;
+
+
+
+initialization
+  CoreSQL  := TSQLDictionary.Create;
+
+finalization
+  CoreSQL.Free;
+
+end.
