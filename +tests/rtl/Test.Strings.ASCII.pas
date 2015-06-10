734c7d70187ffa4725a541e92c4fd03b6c7b6eb5
Jolyon Direnko-Smith <jsmith@deltics.co.nz>
2015-06-10 17:53:33 +1200

    Added ASCII test case

---
+++ +tests/rtl/Test.Strings.ASCII.pas
@@ -0,0 +1,37 @@
+
+  unit Test.Strings.ASCII;
+
+interface
+
+  uses
+    Deltics.Smoketest;
+
+
+  type
+    TASCIIFnTests = class(TTestCase)
+      procedure Transcoding;
+    end;
+
+
+
+implementation
+
+  uses
+    Deltics.Strings,
+    Test.Strings;
+
+
+
+
+
+{ TANSIFnTests }
+
+  { - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - }
+  procedure TASCIIFnTests.Transcoding;
+  begin
+    Test('ASCII.Encode(%s)', [SRCS]).Expect(ASCII.Encode(SRCS)).Equals(SRCASCII);
+  end;
+
+
+
+end.
